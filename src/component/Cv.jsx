e2e6055c8bad52e64ae7eb1b634ed58b08593858
mdshahnawaz786 <mdshahnawaz911@gmail.com>
2023-02-18 19:45:09 +0530

    done

---
+++ src/component/Cv.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Cv = () => {
+  return (
+    <>
+    <h1>Md Shahnawaz</h1>
+    <h4>Software Engineer</h4>
+    <a href="http://www.linkedin.com/in/shahnawaz0786">linkedin</a>
+    <a href="https://github.com/mdshahnawaz786">github</a>
+    <hr/>
+    <h1>Experiance</h1>
+    <h4>Software Engineer at ABC</h4>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolorum ex corporis obcaecati aperiam sint odio labore doloribus eum molestias nulla perferendis tempora dignissimos nam voluptatibus nihil magni, illo dolore?
+    </p>
+    <hr />
+    <h4>Software Engineer at XYZ</h4>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolorum ex corporis obcaecati aperiam sint odio labore doloribus eum molestias nulla perferendis tempora dignissimos nam voluptatibus nihil magni, illo dolore?
+    </p>
+    <hr />
+    <h1>Skills</h1>
+    <ul>
+      <li>Java</li>
+      <li>Javascript</li>
+      <li>Html</li>
+      <li>css</li>
+    </ul>
+    <hr />
+    <h1>Hobbies</h1>
+    <ol>
+      <li>Travelling</li>
+      <li>Reading Books</li>
+      <li>Playing cricket</li>
+      <li>Playing Chess</li>
+    </ol>
+    <hr />
+    </>
+  )
+}
+
+export default Cv
